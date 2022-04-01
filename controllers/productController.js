9d5b1ffaadb4c65cc14211a0b56e1b17e40744f0
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-01 12:32:45 -0300

    feat: Inserir uma condicional de erro na função findById

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,10 +2,20 @@ const productService = require('../services/productService');
 
 const getAll = async (_req, res) => {
   const products = await productService.getAll();
-  // console.log(products);
   res.status(200).json(products);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+
+  const product = await productService.findById(id);
+
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  res.status(200).json(product);
 };
 
 module.exports = {
   getAll,
+  findById,
 };
