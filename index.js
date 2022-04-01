18a0f1deedf7d53a33c359f91779946c51c09935
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-01 12:19:59 -0300

    refactor: refatorar o index contemplando o uso do Router

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
-// const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
+const productRoutes = require('./routes/productRoutes');
+const salesRoutes = require('./routes/salesRoutes');
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRoutes);
+
+app.use('/sales', salesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
