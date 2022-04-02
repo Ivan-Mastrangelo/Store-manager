5567e008ad53c858306055c310c4ddb48ad4c8b2
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-01 22:51:54 -0300

    feat: Implementar a função de insersão de produto ap BD, na camada controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,15 @@ const findById = async (req, res) => {
   res.status(200).json(product);
 };
 
+const create = async (req, res) => {
+  const { name, quantity } = req.body;
+  const newProduct = await productService.create({ name, quantity });
+  
+  return res.status(201).json(newProduct);
+};
+
 module.exports = {
   getAll,
   findById,
+  create,
 };
