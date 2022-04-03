dccd9bb6884121c7faaf2ed997455fa5b1afe18c
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-03 16:41:09 -0300

    feat: implementar as funções para cadastrar e atualizar vendas, na camada service.

--- services/salesService.js
+++ services/salesService.js
@@ -12,7 +12,28 @@ const findById = async (id) => {
   return sale;
 };
 
+const create = async ({ productId, quantity }) => {
+  const newSale = await salesModel.create({ productId, quantity });
+  return newSale;
+};
+
+const update = async (sale) => {
+  const { id, productId, quantity } = sale;
+
+  if (quantity < 1) throw Error('"quantity" must be greater than or equal to 1');
+  
+  const getSale = await salesModel.findById(id);
+ 
+  if (!getSale) throw Error('Product not found');
+
+  const readyUp = await salesModel.update({ id, productId, quantity });
+
+  return readyUp;
+};
+
 module.exports = {
   getAll,
-  findById,   
+  findById,
+  create,
+  update,
 };
