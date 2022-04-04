7dfeecfff12465183115753baa30a2255cc7c3d4
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-04 09:56:42 -0300

    testando os middlewares

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -11,6 +11,6 @@ router.get('/:id', salesController.findById);
 
 router.post('/', salesController.create);
 
-router.put('/:id', productIdIn, quantityExist, quantityplus, salesController.update);
+router.put('/:id', salesController.update);
 
 module.exports = router;
